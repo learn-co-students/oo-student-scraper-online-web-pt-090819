8aeced46010f71c21a0aff409218c6bac33143f2
Eddy Tyannikov <etyannikov@gmail.com>
2019-10-30 00:41:33 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,51 @@
+require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
 class Scraper
-
-  def self.scrape_index_page(index_url)
+  
+  def self.scrape_index_page(html)
+    site = Nokogiri::HTML(open(html))
     
+    students = []
+    
+    #student: "div.student-card"
+    #name: student.css("a div.card-text-container h4").text
+    #location: student.css("a div.card-text-container p").text
+    #profile_url: student.css("a").attribute("href").value
+    
+    site.css("div.student-card").each do |profile| 
+    student = {
+      :name => profile.css("a div.card-text-container h4").text,
+      :location => profile.css("a div.card-text-container p").text,
+      :profile_url => profile.css("a").attribute("href").value
+    }
+    students << student
+  end
+  students
   end
 
-  def self.scrape_profile_page(profile_url)
+  def self.scrape_profile_page(html)
+    site = Nokogiri::HTML(open(html))
     
-  end
-
-end
+    student = {}
+    
+    student[:profile_quote] = site.css("div.profile-quote").text
+    student[:bio] = site.css("div.description-holder p").text
+    
+    site.css("div.social-icon-container a").each do |sic|
+      url = sic.attribute("href").value
+      if url.include? "twitter"
+        student[:twitter] = url
+      elsif url.include? "linkedin"
+        student[:linkedin] = url
+      elsif url.include? "github"
+        student[:github] = url
+      else
+        student[:blog] = url
+      end #elsif test
+    end #each loop
+    student
+  end #scrape_profile_page
+end #scraper
 
